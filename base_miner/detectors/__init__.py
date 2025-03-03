fa805c33fca61ede5c94ccd2a42cc8b9f7c4721d
hayden-yuma <hayden@yumaai.com>
2025-03-03 09:41:48 -0500

    fixed import in __init__.py

--- base_miner/detectors/__init__.py
+++ base_miner/detectors/__init__.py
@@ -1,5 +1,1 @@
 from .feature_detector import FeatureDetector
-from .npr_detector import NPRImageDetector
-from .ucf_detector import UCFImageDetector
-from .camo_detector import CAMOImageDetector
-from .tall_detector import TALLVideoDetector
