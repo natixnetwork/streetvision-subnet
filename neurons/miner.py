e98523356cda13723ee319ed6b286e197cd665e3
hayden-yuma <hayden@yumaai.com>
2025-03-17 14:29:01 -0400

    miner requires detector imports even though not directly called

--- neurons/miner.py
+++ neurons/miner.py
@@ -24,6 +24,7 @@ import typing
 import bittensor as bt
 from PIL import Image
 
+from base_miner.detectors import RoadworkDetector, ViTImageDetector  # noqa: F401
 from base_miner.registry import DETECTOR_REGISTRY
 from natix.base.miner import BaseMinerNeuron
 from natix.protocol import ImageSynapse
