dd6645bcf6e6986fe3700948ffa3d8cc4f8e0bc4
Peter Carlson <peter@yumaai.com>
2025-05-28 10:51:43 -0400

    updates

--- base_miner/config.py
+++ base_miner/config.py
@@ -1,8 +1,8 @@
 from pathlib import Path
-from typing import Any, Dict, List, Optional
+from typing import Dict, List
 import os
 
-HUGGINGFACE_REPO = os.getenv("HUGGINGFACE_REPO", "no_huggingface_repo")
+HUGGINGFACE_REPO = os.getenv("HUGGINGFACE_REPO", "natix-network-org")
 HUGGINGFACE_CACHE_DIR: Path = Path.home() / ".cache" / "huggingface"
 IMAGE_DATASETS: Dict[str, List[Dict[str, str]]] = {
     "Roadwork": [
