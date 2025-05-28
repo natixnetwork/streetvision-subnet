ea5eaffb6d47b3313b166fc4a98a2f75c1ffb0c3
Ali <hagh.a.ext@natix.io>
2025-05-28 08:15:38 +0200

    fix:remove sol

--- register.sh
+++ register.sh
@@ -13,13 +13,13 @@ show_help() {
   echo "┌────────────────────────────────────────────────────────────┐"
   echo -e "│         ${GREEN}NATIX Street Vision economy registration${CYAN}         │"
   echo "├────────────────────────────────────────────────────────────┤"
-  echo -e "│ ${YELLOW}Usage:${CYAN} ./register.sh <uid> <bt_wallet_name> <bt_hotkey_name> <solana_keypair_path> <participant_type> [hf_model_path]"
+  echo -e "│ ${YELLOW}Usage:${CYAN} ./register.sh <uid> <bt_wallet_name> <bt_hotkey_name> <participant_type> [hf_model_path]"
   echo "│"
   echo "│ Example for miner:"
-  echo "│   ./register.sh 10 reyraa default ~/.config/solana/reyraa.json miner reyraa/roadwork"
+  echo "│   ./register.sh 10 reyraa default miner reyraa/roadwork"
   echo "│"
   echo "│ Example for validator:"
-  echo "│   ./register.sh 10 reyraa default ~/.config/solana/reyraa.json validator"
+  echo "│   ./register.sh 10 reyraa default validator"
   echo "│"
   echo "│ This script will:"
   echo "│ - Generate a secure timestamp"
@@ -31,15 +31,14 @@ show_help() {
 }
 
 # Validate args
-if [ "$#" -lt 5 ] || [ "$#" -gt 6 ]; then
+if [ "$#" -lt 4 ] || [ "$#" -gt 5 ]; then
   show_help
 fi
 
 BT_UID=$1
 BT_WALLET=$2
 BT_HOTKEY=$3
-SOLANA_KEYPAIR=$4
-PARTICIPANT_TYPE=$5
+PARTICIPANT_TYPE=$4
 
 echo $PARTICIPANT_TYPE
 
@@ -54,13 +53,6 @@ else
 fi
 
 echo "UID at start: $BT_UID"
-
-# Install missing tools
-command -v solana >/dev/null 2>&1 || {
-  echo -e "${YELLOW}Installing Solana CLI...${RESET}"
-  sh -c "$(curl -sSfL https://release.solana.com/stable/install)"
-  export PATH="$HOME/.local/share/solana/install/active_release/bin:$PATH"
-}
 
 command -v btcli >/dev/null 2>&1 || {
   echo -e "${RED}Error: Bittensor CLI (btcli) not found. Please install it manually.${RESET}"
@@ -79,17 +71,6 @@ command -v jq >/dev/null 2>&1 || {
 TIMESTAMP=$(date +%s)
 echo -e "${GREEN}Generated timestamp:${RESET} $TIMESTAMP"
 
-# Solana signing
-SOLANA_PUBKEY=$(solana address --keypair "$SOLANA_KEYPAIR")
-SOLANA_SIGNATURE=$(solana sign-offchain-message --keypair "$SOLANA_KEYPAIR" --no-address-labels "$TIMESTAMP")
-
-if [[ -z "$SOLANA_SIGNATURE" || -z "$SOLANA_PUBKEY" ]]; then
-  echo -e "${RED}[ERROR]${RESET} Failed to extract Solana signature or public key."
-  exit 1
-fi
-
-echo -e "${GREEN}Generated signature for${RESET} Solana"
-
 # Bittensor signing
 BT_SIGNATURE=$(btcli w sign --wallet-name "$BT_WALLET" --hotkey "$BT_HOTKEY" --use-hotkey --message "$TIMESTAMP" --json-out | jq -r '.signed_message')
 BT_PUBKEY=$(btcli w list --json-out | jq -r ".wallets[] | select(.name==\"$BT_WALLET\") | .hotkeys[] | select(.name==\"$BT_HOTKEY\") | .ss58_address")
@@ -102,15 +83,11 @@ if [ "$PARTICIPANT_TYPE" == "miner" ]; then
     --arg msg "$TIMESTAMP" \
     --arg bt_pk "$BT_PUBKEY" \
     --arg bt_sig "$BT_SIGNATURE" \
-    --arg sol_pk "$SOLANA_PUBKEY" \
-    --arg sol_sig "$SOLANA_SIGNATURE" \
     --arg type "$PARTICIPANT_TYPE" \
     --arg repo "$HF_MODEL" \
     '{
       uid: $uid,
       message: $msg,
-      public_key: $sol_pk,
-      signature: $sol_sig,
       natix_public_key: $bt_pk,
       natix_signature: $bt_sig,
       type: $type,
@@ -123,14 +100,10 @@ else
     --arg msg "$TIMESTAMP" \
     --arg bt_pk "$BT_PUBKEY" \
     --arg bt_sig "$BT_SIGNATURE" \
-    --arg sol_pk "$SOLANA_PUBKEY" \
-    --arg sol_sig "$SOLANA_SIGNATURE" \
     --arg type "$PARTICIPANT_TYPE" \
     '{
       uid: $uid,
       message: $msg,
-      public_key: $sol_pk,
-      signature: $sol_sig,
       natix_public_key: $bt_pk,
       natix_signature: $bt_sig,
       type: $type
